e4ca42ed3aac4d9c56af95638572e19263e37618
Saurabh Kushwaha <saurabhkushwaha697@gmail.com>
2022-07-13 01:36:43 +0530

    loading animation added

---
+++ node_modules/is-program-installed/index.js
@@ -0,0 +1,82 @@
+const { readdirSync } = require('fs')
+const { execSync } = require('child_process')
+
+const opts = {
+  stdio: 'ignore'
+}
+const exec = (cmd) => execSync(cmd, opts)
+
+const isUnixInstalled = (program) => {
+  try {
+    exec(`hash ${program} 2>/dev/null`)
+    return true
+  } catch {
+    return false
+  }
+}
+
+const isDirectory = (path) => {
+  try {
+    readdirSync(path)
+    return true
+  } catch {
+    return false
+  }
+}
+const isDotDesktopInstalled = (program) => {
+  const dirs = [
+    process.env.XDG_DATA_HOME && process.env.XDG_DATA_HOME + '/applications',
+    process.env.HOME && process.env.HOME + '/.local/share/applications',
+    '/usr/share/applications',
+    '/usr/local/share/applications'
+  ]
+    .filter(Boolean)
+    .filter(isDirectory)
+
+  const trimExtension = (x) => x.replace(/\.desktop$/, '')
+  const desktopFiles = dirs
+    .flatMap((x) => readdirSync(x))
+    .filter((x) => x.endsWith('.desktop'))
+    .map(trimExtension)
+
+  const programTrimmed = trimExtension(program)
+  return desktopFiles.includes(programTrimmed)
+}
+
+const isMacInstalled = (program) => {
+  try {
+    exec(`osascript -e 'id of application "${program}"' 2>&1>/dev/null`)
+    return true
+  } catch {
+    return false
+  }
+}
+
+const isWindowsInstalled = (program) => {
+  // Try a couple variants, depending on execution environment the .exe
+  // may or may not be required on both `where` and the program name.
+  const attempts = [
+    `where ${program}`,
+    `where ${program}.exe`,
+    `where.exe ${program}`,
+    `where.exe ${program}.exe`
+  ]
+
+  let success = false
+  for (const a of attempts) {
+    try {
+      exec(a)
+      success = true
+    } catch {}
+  }
+
+  return success
+}
+
+module.exports = (program) =>
+  [
+    isUnixInstalled,
+    isMacInstalled,
+    isWindowsInstalled,
+    isDotDesktopInstalled
+  ].some((f) => f(program))
